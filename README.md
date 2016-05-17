fb4c92d3bfc882379f5f72ae97b988676a7f3fb4
neil patel <neil@myindianweddingapp.com>
2016-05-17 16:38:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+== README
+
+example of TDD using Rspec , Capybara & Database Cleaner
---
+++ spec/models/album_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Album do
+  subject (Album.new)
+
+  it "is not valid without a title" do
+    expect(subject).not_to be_valid
+  end
+
+  it "is not valid with a title longer than 100 characters" do
+    subject.title = 'a' * 101
+    expect(subject).not_to be_valid
+  end
+end
